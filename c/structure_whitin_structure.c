dc3482282a447845e16b33e3d6cbf9fe0c8ca452
Wandeeer <xavistoribi@gmail.com>
2025-06-20 20:38:17 -0600

    news

---
+++ c/structure_whitin_structure.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Fecha {
+  int day;
+  int month;
+  int year;
+};
+struct Persona {
+  char *name;
+  int age;
+  struct Fecha* birthdate;
+};
+
+int main() {
+  int day = 12;
+  int month = 04;
+  int year = 2007;
+
+  char name[10] = "Xavier";
+  int age = 18;
+
+  struct Persona* persona1 = (struct Persona*)malloc(sizeof(struct Persona));
+ // struct Fecha* birthdate_one = (struct Fecha*)malloc(sizeof(struct Fecha));
+
+  persona1->name = name;
+  persona1->age = age;
+   
+  persona1->birthdate->day = day;
+  persona1->birthdate->month = month;
+  persona1->birthdate->year = year;
+
+  printf("Datos de Persona 1: \n");
+  printf("Nombre: %s \n", persona1->name);
+  printf("Edad: %d \n", persona1->age);
+  printf("Cumpleannos: %d/%d/%d", persona1->birthdate->day, persona1->birthdate->month, persona1->birthdate->year);
+
+  free(persona1);
+  //free(birthdate_one);
+
+  return 0;
+}
